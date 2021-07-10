912402a86dd2f27b03252e54886991445120f593
mfdivya21 <mfdivya21@gmail.com>
2021-07-10 15:37:58 +0530

    Add files via upload

---
+++ Authentication/Authentication/src/test/java/com/divyashree/Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyashree.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
